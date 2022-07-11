58c353e7c603deb64a265870b2611109212ee91e
Jose Machado <m4ch4do@protonmail.com>
2022-07-11 16:45:04 -0400

    Add Calculations for age and full name in Customer

--- app_lease/models.py
+++ app_lease/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 
 class Customer(models.Model):
@@ -28,3 +29,29 @@ class Customer(models.Model):
     created_by = models.PositiveIntegerField(blank=True, null=True)
     updated_by = models.PositiveIntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+    # calculations
+    @property
+    def age(self):
+        today = date.today()
+        dob = self.dob
+        diff_in_years = today.year - dob.year
+
+        if today.month < dob.month:
+            has_birthday_passed = False
+        elif today.month > dob.month:
+            has_birthday_passed = True
+        elif today.day >= dob.day:
+            has_birthday_passed = True
+        else:
+            has_birthday_passed = False
+
+        age = diff_in_years if has_birthday_passed else diff_in_years - 1
+
+        return age
+
+    @property
+    def name(self):
+        return self.first_name + " " + self.last_name
