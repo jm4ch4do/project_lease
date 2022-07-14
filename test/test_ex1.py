db7e7419518e6bd89578552ffae5383f86ffd3f7
Jose Machado <m4ch4do@protonmail.com>
2022-07-14 11:30:16 -0400

    Example file for a test

--- test/test_ex1.py
+++ test/test_ex1.py
@@ -1,4 +1,10 @@
 import pytest
+from django.contrib.auth.models import User
 
-def test_example():
-    assert 1 == 1
+@pytest.fixture
+def user_1(db):
+    return User.objects.create_user("test-user")
+
+@pytest.mark.django_db
+def test_set_check_password(user_1):
+    assert user_1.username == "test-user"
