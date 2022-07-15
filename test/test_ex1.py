026111f58b0f8d14b3cd633e8f72efa193756f0a
Jose Machado <m4ch4do@protonmail.com>
2022-07-14 22:24:54 -0400

    Add new file for testing views

--- test/test_ex1.py
+++ test/test_ex1.py
@@ -4,8 +4,8 @@ def test_example(new_user):
     assert True if user.username else False
 
 
-def test_customer(new_customer):
-    customer = new_customer
-    print(customer.first_name)
-    assert True if customer.user.username else False
+# def test_customer(new_customer):
+#     customer = new_customer
+#     print(customer.first_name)
+#     assert True if customer.user.username else False
 
