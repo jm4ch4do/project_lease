c163d25411866d628c5d2d5dad8bbe6c328aed4b
Jose Machado <m4ch4do@protonmail.com>
2022-07-20 07:35:01 -0400

    New lead model. New folder utils containing function for age calculation

--- app_lease/admin.py
+++ app_lease/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import customer, contact
+from .models import customer, contact, lead
 
 admin.site.register(customer.Customer)
 admin.site.register(contact.Contact)
+admin.site.register(lead.Lead)
 
 
---
+++ app_lease/utils/age_from_dob.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+
+def age_from_dob(dob):
+    """" Calculates age using date of birth (dob)"""
+
+    # find years_passed since dob
+    today = date.today()
+    diff_in_years = today.year - dob.year
+
+    # find if bithday already passed the current year
+    if today.month < dob.month:
+        has_birthday_passed = False
+    elif today.month > dob.month:
+        has_birthday_passed = True
+    elif today.day >= dob.day:
+        has_birthday_passed = True
+    else:
+        has_birthday_passed = False
+
+    # decide if age is years_passed or years_passed+1
+    age = diff_in_years if has_birthday_passed else diff_in_years - 1
+
+    return age
