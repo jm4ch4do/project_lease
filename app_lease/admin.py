290910b494f02a2112a8cc0e2924c2517532aa66
Jose Machado <m4ch4do@protonmail.com>
2022-07-23 07:38:49 -0400

    Register service model in admin

--- app_lease/admin.py
+++ app_lease/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Contact, Lead, Vehicle
+from .models import Customer, Contact, Lead, Vehicle, Service
 
 admin.site.register(Customer)
 admin.site.register(Contact)
 admin.site.register(Lead)
 admin.site.register(Vehicle)
-
-
+admin.site.register(Service)
