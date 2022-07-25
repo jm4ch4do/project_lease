0fb5964f48dcf079a71e980f0eac09f2d2fa2f43
Jose Machado <m4ch4do@protonmail.com>
2022-07-25 19:57:08 -0400

    Add docstring for user model tests

--- app_lease/test/user/user_model_hp_test.py
+++ app_lease/test/user/user_model_hp_test.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 @pytest.mark.order(2)
 @pytest.mark.django_db
 def test_create_user():
+    """ User can exist without a related customer """
+
     created_user = random_user()
     assert True if isinstance(created_user, User) else False  # User object created
     assert True if User.objects.all().count() == 1 else False  # only one object in table users
@@ -17,7 +19,8 @@ def test_create_user():
 @pytest.mark.order(2)
 @pytest.mark.django_db
 def test_delete_user():
-    # deleting user should delete user and customer
+    """ Deleting user deletes related customer """
+
     created_customer = random_customer()
     User.objects.all().delete()
     assert True if User.objects.all().count() == 0 else False
