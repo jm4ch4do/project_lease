2cb45e91ba5199935a6d0ef458ced7e2d09aad66
Jose Machado <m4ch4do@protonmail.com>
2022-07-25 10:04:04 -0400

    Add happy path tests for contacts

--- app_lease/test/contacts/contacts_model_hp_test.py
+++ app_lease/test/contacts/contacts_model_hp_test.py
@@ -6,9 +6,8 @@ from django.contrib.auth.models import User
 
 
 @pytest.mark.django_db
-def test_create_contact():
+def test_create_contact_with_email_for_customer():
 
-    # create contact with email for a customer
     created_contact = random_contact()
     assert True if isinstance(created_contact, Contact) else False  # Contact object created
     assert True if Contact.objects.all().count() == 1 else False  # only one object in table contact
@@ -17,28 +16,37 @@ def test_create_contact():
     assert True if not created_contact.lead else False  # contact has no lead
     assert True if created_contact.email else False  # contact has an email
     assert True if not created_contact.phone else False  # contact has no phone
-    assert True if created_contact.type == 1 else False # contact has type 1 (email)
+    assert True if created_contact.type == 1 else False  # contact has type 1 (email)\
 
-    # create contact with phone for a customer
+
+@pytest.mark.django_db
+def test_create_contact_with_phone_for_customer():
+
     created_contact = random_contact(contact_type=2)
     assert True if isinstance(created_contact, Contact) else False  # Contact object created
-    assert True if Contact.objects.all().count() == 2 else False  # only two objects in table contact
-    assert True if Customer.objects.all().count() == 2 else False  # only two objects in table customer
+    assert True if Contact.objects.all().count() == 1 else False  # only two objects in table contact
+    assert True if Customer.objects.all().count() == 1 else False  # only two objects in table customer
     assert True if isinstance(created_contact.customer, Customer) else False  # contact has valid customer
     assert True if not created_contact.lead else False  # contact has no lead
     assert True if created_contact.phone else False  # contact has a phone
     assert True if not created_contact.email else False  # contact has no email
     assert True if created_contact.type == 2 else False  # contact has type 2 (phone)
 
-    # create contact with email for a lead
+
+@pytest.mark.django_db
+def test_create_contact_with_email_for_lead():
+
     created_contact = random_contact(related_to='lead')
     assert True if isinstance(created_contact, Contact) else False  # Contact object created
     assert True if isinstance(created_contact.lead, Lead) else False  # contact has valid lead
     assert True if not created_contact.customer else False  # contact has no customer
     assert True if created_contact.email else False  # contact has an email
     assert True if not created_contact.phone else False  # contact has no phone
-    assert True if created_contact.type == 1 else False # contact has type 1 (email)
+    assert True if created_contact.type == 1 else False  # contact has type 1 (email)
 
+
+@pytest.mark.django_db
+def test_create_contact_with_phone_for_lead():
     # create contact with phone for a lead
     created_contact = random_contact(related_to='lead', contact_type=2)
     assert True if isinstance(created_contact, Contact) else False  # Contact object created
@@ -49,11 +57,39 @@ def test_create_contact():
     assert True if created_contact.type == 2 else False  # contact has type 2 (phone)
 
 
-# create contact
-# contact delete should not delete customer or lead
-# customer delete should delete contact
-# lead delete should delete contact
-# contact string output
+@pytest.mark.django_db
+def test_delete_contact_dont_delete_related_customer():
+
+    created_contact = random_contact()  # also creates a related customer and user
+    created_contact.delete()
+    assert True if Customer.objects.all().count() == 1 else False  # customer was not removed
+    assert True if User.objects.all().count() == 1 else False  # user was not removed
+
+
+@pytest.mark.django_db
+def test_delete_contact_dont_delete_related_lead():
+
+    created_contact = random_contact(related_to='lead')  # also creates a related customer and user
+    created_contact.delete()
+    assert True if Lead.objects.all().count() == 1 else False  # lead was not removed
+
+
+@pytest.mark.django_db
+def test_delete_customer_delete_contact():
+
+    created_contact = random_contact()
+    related_customer = created_contact.customer
+    related_customer.delete()
+    assert True if Contact.objects.all().count() == 0 else False  # customer was not removed
+
+
+@pytest.mark.django_db
+def test_custom_contact():
+    # testing custom methods in customer
+    created_contact = random_contact()
+    assert True if isinstance(str(random_contact), str) else False  # object returns valid string
+
+
 
 
 # sp
@@ -62,20 +98,3 @@ def test_create_contact():
 # put letter in the phone
 # put too much or too little numbers in the phone
 # put false email in emails
-
-
-
-# @pytest.mark.django_db
-# def test_delete_lead():
-#     created_lead = random_lead()
-#     # deleting lead should clear lead table
-#     Lead.objects.all().delete()
-#     assert True if Lead.objects.all().count() == 0 else False
-#
-#
-# @pytest.mark.django_db
-# def test_custom_lead():
-#     # testing custom methods in customer
-#     created_lead = random_lead()
-#     assert True if isinstance(str(random_lead), str) else False  # object returns valid string
-#     assert True if isinstance(created_lead.age, int) else False  # age return valid int
