3cbb9858f08ad2ed698b4a4462cca398682ae2ee
Jose Machado <m4ch4do@protonmail.com>
2022-07-25 12:10:54 -0400

    Add sp test for email and phone

--- app_lease/test/contacts/contacts_model_sp_test.py
+++ app_lease/test/contacts/contacts_model_sp_test.py
@@ -5,6 +5,7 @@ from app_lease.test.generator import random_contact, random_lead
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
+from faker import Faker
 
 
 @pytest.mark.django_db
@@ -39,9 +40,66 @@ def test_customer_cant_have_related_contact_and_lead():
     assert True if exp else False
 
 
-# sp
-# create contact without customer or lead
-# create contact without phone or email
-# put letter in the phone
-# put too much or too little numbers in the phone
-# put false email in emails
+@pytest.mark.django_db
+def test_customer_need_related_email():
+
+    created_contact = random_contact()
+    with pytest.raises(ValidationError) as exp:
+        created_contact.email = None
+        created_contact.full_clean()
+    assert True if exp else False
+
+
+@pytest.mark.django_db
+def test_customer_need_related_phone():
+
+    created_contact = random_contact(contact_type=2)
+    with pytest.raises(ValidationError) as exp:
+        created_contact.phone = None
+        created_contact.full_clean()
+    assert True if exp else False
+
+
+@pytest.mark.django_db
+def test_customer_cant_have_related_contact_and_lead():
+
+    fake = Faker()
+    created_contact = random_contact()
+    random_phone = int("1" + str(fake.msisdn()[3:]))
+
+    with pytest.raises(ValidationError) as exp:
+        created_contact.phone = random_phone
+        created_contact.full_clean()
+    assert True if exp else False
+
+
+@pytest.mark.django_db
+def test_customer_bad_phone():
+
+    fake = Faker()
+    created_contact = random_contact(contact_type=2)
+    random_phone = "1" + str(fake.msisdn()[3:])
+    random_phone = int(random_phone[4:])
+
+    with pytest.raises(ValidationError) as exp:
+        created_contact.phone = random_phone
+        created_contact.full_clean()
+    assert True if exp else False
+
+
+@pytest.mark.django_db
+def test_customer_bad_phone():
+
+    created_contact = random_contact()
+
+    random_email_no_at_sign = 'testing.com'
+    with pytest.raises(ValidationError) as exp:
+        created_contact.email = random_email_no_at_sign
+        created_contact.full_clean()
+    assert True if exp else False
+
+    random_email_no_dot = 'testing@testingcom'
+    with pytest.raises(ValidationError) as exp:
+        created_contact.email = random_email_no_dot
+        created_contact.full_clean()
+    assert True if exp else False
