8d70fb854787e279d9d3e3dab25c9115cfd504e6
Jose Machado <m4ch4do@protonmail.com>
2022-07-25 11:54:56 -0400

    Add validator for exclusive fields

--- app_lease/validators.py
+++ app_lease/validators.py
@@ -1,8 +1,8 @@
 from django.core.exceptions import ValidationError
 
 
-# at_least one of the fields must have a value
 def at_least_one_required(fields, noun):
+    """ At least one of the fields must have a value """
 
     # if at least a field has a value -> OK
     for value in fields:
@@ -16,5 +16,25 @@ def at_least_one_required(fields, noun):
             code='at_least_one',
             params={'noun': noun}
         )
+
+    return fields\
+
+
+
+def exclusive_fields(fields, noun):
+    """ If one field has a value the other can't have a value """
+
+    # count of values in fields can't be higher than one
+    count = 0
+    for value in fields:
+        if value:
+            count += 1
+
+        if count > 1:
+            raise ValidationError(
+                "You can have only one of this values set %(noun)s",
+                code='exclusive_fields',
+                params={'noun': noun}
+            )
 
     return fields\
