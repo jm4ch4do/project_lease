fb83b075cbd8d0b087caa1a1edf9ed138c18d526
Jose Machado <m4ch4do@protonmail.com>
2022-07-25 19:46:22 -0400

    Add docstring for lead model tests

--- app_lease/test/leads/lead_model_hp_test.py
+++ app_lease/test/leads/lead_model_hp_test.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 @pytest.mark.order(4)
 @pytest.mark.django_db
 def test_create_lead():
+    """ Test lead creation which doesn't affect any other table """
+
     created_lead = random_lead()
     assert True if isinstance(created_lead, Lead) else False  # Lead object created
     assert True if Lead.objects.all().count() == 1 else False  # only one object in table lead
@@ -16,6 +18,8 @@ def test_create_lead():
 @pytest.mark.order(4)
 @pytest.mark.django_db
 def test_delete_lead():
+    """ Basic test for deleting lead """
+
     created_lead = random_lead()
     # deleting lead should clear lead table
     Lead.objects.all().delete()
@@ -25,7 +29,8 @@ def test_delete_lead():
 @pytest.mark.order(4)
 @pytest.mark.django_db
 def test_custom_lead():
-    # testing custom methods in customer
+    """ Testing custom methods in lead """
+
     created_lead = random_lead()
     assert True if isinstance(str(random_lead), str) else False  # object returns valid string
     assert True if isinstance(created_lead.age, int) else False  # age return valid int
