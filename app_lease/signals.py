48939acd27d628ca7445c67564c7ed091019fc75
Jose Machado <m4ch4do@protonmail.com>
2022-07-30 16:03:17 -0400

    Add signal for creating an invoice when a trade is created if service is lease

--- app_lease/signals.py
+++ app_lease/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_delete, pre_delete
-from app_lease.models import Customer, Proposal
+from django.db.models.signals import post_delete, post_init
+from app_lease.models import Customer, Trade, Invoice
 from django.contrib.auth.models import User
 
 
@@ -11,6 +11,15 @@ def customer_delete_handler(sender, instance, *args, **kwargs):
     User.objects.get(pk=user_id).delete()
 
 
+# When a trade is created, a proposal should also be created is trade is lease
+@receiver(post_init, sender=Trade)
+def trade_create_handler(sender, instance, *args, **kwargs):
+    created_trade = instance
 
-
-
+    if created_trade.service.service_type == 1:
+        # create invoice
+        Invoice.objects.create(
+            trade=created_trade,
+            customer=created_trade.vehicle.customer,
+            amount=created_trade.service.cost,
+        )
