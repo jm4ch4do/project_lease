3627867dff6c7e5a44c7c5bad9794e1e16be9200
Jose Machado <m4ch4do@protonmail.com>
2022-07-30 22:09:46 -0400

    Revert test_create_invoice_from_trade. It has to be done in Controller

--- app_lease/signals.py
+++ app_lease/signals.py
@@ -2,28 +2,10 @@ from django.dispatch import receiver
 from django.db.models.signals import post_delete, post_init
 from app_lease.models import Customer, Trade, Invoice
 from django.contrib.auth.models import User
-
+from django.db import transaction, IntegrityError
 
 # When a Customer is deleted, we should also delete the related User
 @receiver(post_delete, sender=Customer)
 def customer_delete_handler(sender, instance, *args, **kwargs):
     user_id = instance.user.id
     User.objects.get(pk=user_id).delete()
-
-
-# When a trade is created, a proposal should also be created is trade is lease
-# @receiver(post_init, sender=Trade)
-# def trade_create_handler(sender, instance, *args, **kwargs):
-#     created_trade = instance
-#
-#     if created_trade.service.service_type == 1:
-#         # create invoice
-#         Invoice.objects.create(
-#             trade=created_trade,
-#             customer=created_trade.vehicle.customer,
-#             amount=created_trade.service.cost,
-#             system_note='for accepting proposal for service sale'
-#         )
-
-
-
--- app_lease/test/invoice/invoice_model_hp_test.py
+++ app_lease/test/invoice/invoice_model_hp_test.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 
 @pytest.mark.order(10)
-@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True)
 def test_create_invoice():
     """ Creating invoice also creates related trade and customer """
 
@@ -67,19 +67,6 @@ def test_delete_invoice_from_customer():
 
 
 
-# @pytest.mark.order(10)
-# @pytest.mark.django_db
-# def test_create_invoice_from_trade():
-#     """ An invoice must be created when creating trade for lease service """
-#
-#     created_service = random_service(service_type=1)
-#     created_trade = random_trade(service=created_service)
-#
-#     assert True if Invoice.objects.all().count() == 1 else False
-#
-#     created_invoice = Invoice.objects.all().first()
-#     assert True if created_invoice.trade == created_trade else False
-#     assert True if created_invoice.customer == created_trade.vehicle.customer else False
 
 
 
