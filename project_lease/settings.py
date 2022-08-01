07490653de419bd176af27266280fa959c0ae865
Jose Machado <m4ch4do@protonmail.com>
2022-08-01 10:23:22 -0400

    Changed settings to postgres

--- project_lease/settings.py
+++ project_lease/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'project_lease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db.postgres.lease',
+        'USER': 'postgres',
+        'PASSWORD': 'Teclado123*',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
