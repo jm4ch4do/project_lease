1bfd65e99a39e2706975384899b42938a941d36e
Jose Machado <m4ch4do@protonmail.com>
2022-08-03 15:38:17 -0400

    Add support for get and post in view

--- app_lease/views.py
+++ app_lease/views.py
@@ -1,16 +1,16 @@
 from django.http import JsonResponse
 from app_lease.models import Customer
 from app_lease.serializers import CustomerSerializer
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET', 'POST'])
 def customer_list(request):
 
-    # get all drinks
-    customers = Customer.objects.all()
+    if request.method == 'GET':
 
-    # serialize them
-    serializer = CustomerSerializer(customers, many=True)
+        customers = Customer.objects.all()  # get all drinks
+        serializer = CustomerSerializer(customers, many=True)  # serialize them
+        return JsonResponse({'customers': serializer.data})  # return json
 
-    # return json
-    # return JsonResponse(serializer.data, safe=False)
-    return JsonResponse({'customers': serializer.data})
+    
