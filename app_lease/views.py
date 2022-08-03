2c5a0d49534f314ea7133e2acaa64c5270619b6a
Jose Machado <m4ch4do@protonmail.com>
2022-08-03 14:49:37 -0400

    Add view to return serializer

--- app_lease/views.py
+++ app_lease/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from app_lease.models import Customer
+from app_lease.serializers import CustomerSerializer
 
-# Create your views here.
+
+def drink_list(request):
+
+    # get all drinks
+    customers = Customer.objects.all()
+
+    # serialize them
+    serializer = CustomerSerializer(customers, many=True)
+
+    # return json
+    return JsonResponse(serializer.data)
