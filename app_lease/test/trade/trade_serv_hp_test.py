b275249594787e10342b3c76f8b9e0e6daf1507d
Jose Machado <m4ch4do@protonmail.com>
2022-08-03 12:32:29 -0400

    Minor

--- app_lease/test/trade/trade_serv_hp_test.py
+++ app_lease/test/trade/trade_serv_hp_test.py
@@ -1,7 +1,7 @@
 import pytest
-from app_lease.test.generator import random_trade, random_service, random_vehicle
+from app_lease.test.generator import random_proposal, random_customer, random_service, random_vehicle
 from app_lease.models import Invoice
-from app_lease.services.trade_serv import create_trade
+from app_lease.services.trade_serv import create_trade, cancel_trade
 
 
 @pytest.mark.order(8)
@@ -22,3 +22,37 @@ def test_create_trade_and_related_invoice_for_type_lease():
     assert True if created_invoice.trade == created_trade else False
     assert True if created_invoice.trade.service == created_service else False
     assert True if created_invoice.amount == created_service.cost else False
+
+
+@pytest.mark.order(8)
+@pytest.mark.django_db
+def test_cancel_trade():
+    """ Canceling a trade closes related proposals """
+
+    # two customers come to shop
+    created_customer2 = random_customer()
+    created_customer3 = random_customer()
+
+    # owner makes a proposal
+    created_proposal1 = random_proposal()
+    owner = created_proposal1.trade.vehicle.customer
+
+    # customer2 and customer3 make two proposals
+    created_proposal2 = random_proposal(trade=created_proposal1.trade, created_by_customer=created_customer2)
+    created_proposal3 = random_proposal(trade=created_proposal1.trade, created_by_customer=created_customer3)
+
+    # trade is canceled
+    cancel_trade(created_proposal1.trade)
+
+    # refresh model from database
+    created_proposal1.refresh_from_db(), created_proposal2.refresh_from_db(), created_proposal3.refresh_from_db()
+
+    # ----- results
+    # trade is canceled and proposals are closed
+    assert True if created_proposal1.trade.status == 3 else False
+    assert True if created_proposal1._status == 4 else False
+    assert True if created_proposal2._status == 4 else False
+    assert True if created_proposal3._status == 4 else False
+    assert True if isinstance(created_proposal1.system_note, str) else False
+    assert True if isinstance(created_proposal2.system_note, str) else False
+    assert True if isinstance(created_proposal3.system_note, str) else False
