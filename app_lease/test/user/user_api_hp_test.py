bd25d86da0c87dab11ee5b454dea4e184839e0a3
Jose Machado <m4ch4do@protonmail.com>
2022-08-15 08:14:48 -0400

    Test staff member can update any password

--- app_lease/test/user/user_api_hp_test.py
+++ app_lease/test/user/user_api_hp_test.py
@@ -52,15 +52,48 @@ def test_register_user():
 @pytest.mark.order(2)
 @pytest.mark.django_db
 def test_password_update_own_user():
-    """ A user can update his own password, also staff members and superuser can """
+    """ A user can update his own password """
 
     # create user with related customer
     created_user = random_user(is_active=1)
     created_customer = random_customer(user=created_user)
 
-    # configure token
+    # configure token for created_user
     client = APIClient()
     token, created = Token.objects.get_or_create(user=created_user)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+
+    # make request for changing password
+    new_password = "NewPassword123"
+    url = reverse('api_password_update', kwargs={'pk': created_user.pk})
+    payload = dict(password=new_password)
+    response = client.put(url, payload)
+
+    # verify response is correct
+    assert response.data.get('response') is not None
+    assert response.status_code == 200
+
+    # verify password was indeed changed correctly
+    assert User.objects.first().check_password(new_password)
+
+
+@pytest.mark.order(2)
+@pytest.mark.django_db
+def test_password_update_staff_user():
+    """ A staff member can update any user's password"""
+
+    # create user with related customer
+    created_user = random_user(is_active=1)
+    created_customer = random_customer(user=created_user)
+
+    # create user as member of the staff
+    staff_user = random_user(is_active=1)
+    staff_user.is_staff = 1
+    staff_user.save()
+
+    # configure token for staff_user
+    client = APIClient()
+    token, created = Token.objects.get_or_create(user=staff_user)
     client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
 
     # make request for changing password
@@ -80,17 +113,34 @@ def test_password_update_own_user():
 @pytest.mark.skip
 @pytest.mark.order(2)
 @pytest.mark.django_db
-def test_password_update_staff_user():
-    """ A staff member can can update any user's password"""
-    pass
-
-
-@pytest.mark.skip
-@pytest.mark.order(2)
-@pytest.mark.django_db
 def test_password_update_superuser_user():
-    """ A superuser can can update any user's password"""
-    pass
+    """ A superuser can update any user's password"""
+    # create user with related customer
+    created_user = random_user(is_active=1)
+    created_customer = random_customer(user=created_user)
+
+    # create user as member of the staff
+    staff_user = random_user(is_active=1)
+    staff_user.is_staff = 1
+    staff_user.save()
+
+    # configure token for staff_user
+    client = APIClient()
+    token, created = Token.objects.get_or_create(user=staff_user)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+
+    # make request for changing password
+    new_password = "NewPassword123"
+    url = reverse('api_password_update', kwargs={'pk': created_user.pk})
+    payload = dict(password=new_password)
+    response = client.put(url, payload)
+
+    # verify response is correct
+    assert response.data.get('response') is not None
+    assert response.status_code == 200
+
+    # verify password was indeed changed correctly
+    assert User.objects.first().check_password(new_password)
 
 
 
