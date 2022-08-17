6b75507b60a5c83dde08910c71bb11cf4c2c514d
Jose Machado <m4ch4do@protonmail.com>
2022-08-17 12:08:22 -0400

    Split tests for superuser and staff

--- app_lease/test/user/user_api_list_hp_test.py
+++ app_lease/test/user/user_api_list_hp_test.py
@@ -7,8 +7,8 @@ from rest_framework.authtoken.models import Token
 
 @pytest.mark.order(2)
 @pytest.mark.django_db
-def test_list_user():
-    """ staff members or superusers can get the list of users """
+def test_staff_gets_list_user():
+    """ staff members can get the list of users """
 
     # create user
     created_user = random_user(is_active=1)
@@ -36,6 +36,32 @@ def test_list_user():
     assert not response.data[0].get('token')
 
 
-# a regular user can't get user list
-# staff and superuser need to be authenticated
-# staff and superuser can't be inactive
+@pytest.mark.order(2)
+@pytest.mark.django_db
+def test_superuser_gets_list_user():
+    """ superusers members can get the list of users """
+
+    # create user
+    created_user = random_user(is_active=1)
+    created_user.is_superuser = True
+    created_user.save()
+
+    # create related customer
+    created_customer = random_customer(user=created_user)
+
+    # configure token for created_user
+    client = APIClient()
+    token, created = Token.objects.get_or_create(user=created_user)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+
+    # make request
+    url = reverse("user_list")
+    response = client.get(url)
+
+    # get data back
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert isinstance(response.data[0]['username'], str)
+    assert isinstance(response.data[0]['customer_id'], int)
+    assert not response.data[0].get('password')
+    assert not response.data[0].get('token')
