3ecb36a43a2b55c4688c13e44ed4498581229a74
Jose Machado <m4ch4do@protonmail.com>
2022-08-18 13:47:54 -0400

    Add test superuser gets list of all customers

--- app_lease/test/customer/customer_api_hp_test.py
+++ app_lease/test/customer/customer_api_hp_test.py
@@ -8,7 +8,7 @@ from rest_framework.authtoken.models import Token
 @pytest.mark.order(2)
 @pytest.mark.django_db
 def test_staff_get_customer_list():
-    """ A staff member can't get the list of all customers """
+    """ A staff member can get the list of all customers """
 
     # create staff member
     staff_user = random_user(is_active=1)
@@ -21,6 +21,35 @@ def test_staff_get_customer_list():
     # configure token for staff_user
     client = APIClient()
     token, created = Token.objects.get_or_create(user=staff_user)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+
+    # make request for customer list
+    url = reverse("customers")
+    response = client.get(url)
+
+    # response has the correct values
+    assert response.status_code == 200
+    assert isinstance(response.data[0]['id'], int)
+    assert response.data[0]['user']
+    assert response.data[0]['first_name']
+
+
+@pytest.mark.order(2)
+@pytest.mark.django_db
+def test_superuser_get_customer_list():
+    """ A superuser can get the list of all customers """
+
+    # create superuser
+    super_user = random_user(is_active=1)
+    super_user.is_staff = True
+    super_user.save()
+
+    # create customer
+    random_customer()
+
+    # configure token for staff_user
+    client = APIClient()
+    token, created = Token.objects.get_or_create(user=super_user)
     client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
 
     # make request for customer list
