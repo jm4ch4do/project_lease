bf147f2cfe8fbb0690dc373adef7ac17464fe738
Jose Machado <m4ch4do@protonmail.com>
2022-08-18 06:49:04 -0400

    Complete all hp test for get user

--- app_lease/test/user/user_api_get_hp_test.py
+++ app_lease/test/user/user_api_get_hp_test.py
@@ -35,7 +35,7 @@ def test_get_own_user_details():
 
 @pytest.mark.order(2)
 @pytest.mark.django_db
-def test_staff_gets_a_user_details():
+def test_staff_gets_any_user_details():
     """ A staff member can get details on any user """
 
     # create user
@@ -43,7 +43,7 @@ def test_staff_gets_a_user_details():
 
     # create staff
     staff_user = random_user(is_active=1)
-    staff_user.is_staff = 1
+    staff_user.is_staff = True
     staff_user.save()
 
     # configure token for staff
@@ -68,7 +68,7 @@ def test_staff_gets_a_user_details():
 
 @pytest.mark.order(2)
 @pytest.mark.django_db
-def test_superuser_gets_a_user_details():
+def test_superuser_gets_any_user_details():
     """ A superuser can get details on any user """
 
     # create user
@@ -76,7 +76,7 @@ def test_superuser_gets_a_user_details():
 
     # create superuser
     super_user = random_user(is_active=1)
-    super_user.is_staff = 1
+    super_user.is_superuser = True
     super_user.save()
 
     # configure token for superuser
@@ -99,16 +99,80 @@ def test_superuser_gets_a_user_details():
     assert "password" not in response.data
 
 
+@pytest.mark.order(2)
+@pytest.mark.django_db
+def test_superuser_gets_any_user_details():
+    """ A superuser can get details on any superuser """
+
+    # create user
+    created_user = random_user(is_active=1)
+    created_user.is_superuser = True
+    created_user.save()
+
+    # create superuser
+    super_user = random_user(is_active=1)
+    super_user.is_superuser = True
+    super_user.save()
+
+    # configure token for superuser
+    client = APIClient()
+    token, created = Token.objects.get_or_create(user=super_user)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+
+    # make request for getting user details
+    url = reverse("user_edit", kwargs={'pk': created_user.id})
+    response = client.get(url)
+
+    # response has the correct values
+    assert response.status_code == 200
+    assert response.data.get("email")
+    assert response.data.get("username")
+    assert response.data.get("is_active") is not None
+    assert response.data.get("is_staff") is not None
+    assert response.data.get("customer_id") is not None
+    assert response.data.get('id')
+    assert "password" not in response.data
 
 
+@pytest.mark.order(2)
+@pytest.mark.django_db
+def test_superuser_gets_any_staff_details():
+    """ A superuser can get details on any staff member """
 
-# can get own user
-# can get user as staff member
-# can get user as superuser
+    # create user
+    created_user = random_user(is_active=1)
+    created_user.is_staff = 1
+    created_user.save()
 
+    # create superuser
+    super_user = random_user(is_active=1)
+    super_user.is_superuser = 1
+    super_user.save()
+
+    # configure token for superuser
+    client = APIClient()
+    token, created = Token.objects.get_or_create(user=super_user)
+    client.credentials(HTTP_AUTHORIZATION='Token ' + str(token))
+
+    # make request for getting user details
+    url = reverse("user_edit", kwargs={'pk': created_user.id})
+    response = client.get(url)
+
+    # response has the correct values
+    assert response.status_code == 200
+    assert response.data.get("email")
+    assert response.data.get("username")
+    assert response.data.get("is_active") is not None
+    assert response.data.get("is_staff") is not None
+    assert response.data.get("customer_id") is not None
+    assert response.data.get('id')
+    assert "password" not in response.data
+
+
+# staff user can't edit another staff or a superuser
 # can't get user if not authenticate
 # can't get user if user doesn't exist
 # can't get user if not own user
 
 # staff user can't edit another staff user, only regular users
-# superuser can edit staff users and other superusers
+
